dc0404b1f042ad3f08b98d8bb96e77d1ec4ad96c
Matamoros <23151319@aguascalientes.tecnm.mx>
2026-09-01 09:29:49 -0600

    se agrego clase estudiante a la practica 2

---
+++ Practica2_Estructura/estudiantes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2_Estructura
+{
+    internal class estudiantes
+    {
+        internal class Estudiante
+        {
+            string Nombre;
+            string Carrera;
+            int[] calificaciones;
+
+            public Estudiante(string nombre, string carrera, int num_calif)
+            {
+                this.Nombre = nombre;
+                this.Carrera = carrera;
+                this.calificaciones = new int[num_calif];
+            }
+            public void CapturaCalificacion(int materia, int calif)
+            {
+                calificaciones[materia] = calif;
+            }
+            public int getPromedio()
+            {
+                int suma = 0;
+                int promedio = 0;
+                for (int i = 0; i < calificaciones.Length; i++)
+                {
+                    suma += calificaciones[i];
+                }
+                promedio = suma / calificaciones.Length;
+                return promedio;
+            }
+            public bool Esregular()
+            {
+                for (int i = 0; i < calificaciones.Length; i++)
+                {
+                    if (calificaciones[i] < 70)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+        }
+    }
+
+}
+
