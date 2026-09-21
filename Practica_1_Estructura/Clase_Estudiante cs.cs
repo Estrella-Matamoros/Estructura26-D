62353e4a838aabdad419ae8dd6fb1bdcdc7a2207
Matamoros <23151319@aguascalientes.tecnm.mx>
2026-09-21 09:16:24 -0600

    n

---
+++ Practica_1_Estructura/Clase_Estudiante cs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1_Estructura
+{
+    internal class Clase_Estudiante_cs
+    {
+    }
+}
