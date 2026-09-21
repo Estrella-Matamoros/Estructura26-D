46fda205f2347a33ca57408855951af968160207
Matamoros <23151319@aguascalientes.tecnm.mx>
2026-09-21 09:09:58 -0600

    practica 5

--- Practica2_Estructura/Program.cs
+++ Practica2_Estructura/Program.cs
@@ -4,63 +4,313 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace practica1._0
+
+
+namespace Practica2_Estructura
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int opcion = 0;
-                Console.WriteLine("SELECCIONA UNA OPCION ");
-                Console.WriteLine("1- AGREGAR UN ESTUDIANTE");
-                Console.WriteLine("2- CONSULTAR EL ESTUDIANTE");
-                Console.WriteLine("3- SALIR");
+            estudiantes.Estudiante[] grupo =
+                new estudiantes.Estudiante[0];
 
-                Console.Write("opcion: ");
-            opcion = Convert.ToInt32(Console.ReadLine());
+            int opcion;
 
-            switch (opcion)
+            do
             {
-                case 1: capturarEstudiante();
-                    break;
+                Console.Clear();
+
+               
+                Console.WriteLine(" GRUPO DE ESTUDIANTES");
+               
+                Console.WriteLine("1. AGREGAR UN ESTUDIANTE");
+                Console.WriteLine("2. CONSULTAR ESTUDIANTES");
+                Console.WriteLine("3. CONSULTAR PROMEDIO DEL GRUPO");
+                Console.WriteLine("4. MOSTRAR ESTUDIANTES REGULARES");
+                Console.WriteLine("5. MOSTRAR ESTUDIANTES IRREGULARES");
+                Console.WriteLine("6. SALIR");
+              
+                Console.Write("Opcion: ");
+
+                opcion = Convert.ToInt32(Console.ReadLine());
+                //MODIFICASION DE LA PRACTICA 5 
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+
+                            Console.WriteLine(" AGREGAR ESTUDIANTE ");
+
+                            Console.Write("Nombre: ");
+                            string nombre = Console.ReadLine();
+
+                            Console.Write("Carrera: ");
+                            string carrera = Console.ReadLine();
+
+                            Console.Write("Numero de materias: ");
+                            int numMaterias =
+                                Convert.ToInt32(Console.ReadLine());
+
+                            estudiantes.Estudiante nuevo =
+                                new estudiantes.Estudiante(
+                                    nombre,
+                                    carrera,
+                                    numMaterias
+                                );
+
+                            for (int i = 0; i < numMaterias; i++)
+                            {
+                                Console.Write(
+                                    "Calificacion de la materia "
+                                    + (i + 1) + ": "
+                                );
+
+                                int calificacion =
+                                    Convert.ToInt32(Console.ReadLine());
+
+                                nuevo.CapturaCalificacion(
+                                    i,
+                                    calificacion
+                                );
+                            }
+
+                            estudiantes.Estudiante[] nuevoGrupo =
+                                new estudiantes.Estudiante[
+                                    grupo.Length + 1
+                                ];
+
+                            for (int i = 0; i < grupo.Length; i++)
+                            {
+                                nuevoGrupo[i] = grupo[i];
+                            }
+
+                            nuevoGrupo[nuevoGrupo.Length - 1] = nuevo;
+
+                            grupo = nuevoGrupo;
+
+                            Console.WriteLine();
+                            Console.WriteLine(
+                                "Estudiante agregado correctamente."
+                            );
+
+                            Console.WriteLine();
+                            Console.WriteLine(
+                                "Presiona enter para continuar..."
+                            );
+                            Console.ReadLine();
+
+                            break;
                         }
-        
 
-            Console.ReadLine();
-            }
-        static  estudiantes capturarEstudiante()
-        {
-            //aqui piden el nombre y la carrera 
-            string nombre, carrera;
-            int num_materias;
-           
-            Console.WriteLine("introduce tu nombre: " );
-            nombre = Console.ReadLine();
-            
-            Console.WriteLine("introduce tu carrera: ");
-            carrera = Console.ReadLine();
+                    case 2:
+                        {
+                            Console.Clear();
 
-            Console.WriteLine("introduce numero de materias: ");
-            num_materias = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(
+                                "** DATOS DEL GRUPO **"
+                            );
+                            Console.WriteLine();
 
+                            if (grupo.Length == 0)
+                            {
+                                Console.WriteLine(
+                                    "No hay estudiantes registrados."
+                                );
+                            }
+                            else
+                            {
+                                for (int i = 0; i < grupo.Length; i++)
+                                {
+                                    Console.WriteLine(
+                                        "ESTUDIANTE #" + (i + 1)
+                                    );
 
-            //creamos un objeto de tipo estudiante 
-            estudiantes estudiante_nuevo = new estudiantes(nombre, carrera, num_materias);
-            int calificacion = 0;
-            for (int i = 0; i < num_materias;  i ++)
-            {
-                Console.WriteLine("calificacion de la materia" + (i + 1));
-                calificacion = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(
+                                        "Promedio: "
+                                        + grupo[i].getPromedio()
+                                    );
 
-                estudiante_nuevo.CapturaCalificacion(i, calificacion);
+                                    Console.WriteLine(
+                                        "Estatus: "
+                                        + (grupo[i].Esregular()
+                                            ? "REGULAR"
+                                            : "IRREGULAR")
+                                    );
 
-            }
+                                    Console.WriteLine(
+                                        "----------------------------"
+                                    );
+                                }
+                            }
 
-            
+                            Console.WriteLine();
+                            Console.WriteLine(
+                                "Presiona ENTER para continuar..."
+                            );
+                            Console.ReadLine();
 
-            return estudiante_nuevo;
+                            break;
+                        }
 
-        }
+                    case 3:
+                        {
+                            Console.Clear();
+
+                            Console.WriteLine(
+                                "** PROMEDIO DEL GRUPO **"
+                            );
+
+                            if (grupo.Length == 0)
+                            {
+                                Console.WriteLine(
+                                    "No hay estudiantes registrados."
+                                );
+                            }
+                            else
+                            {
+                                double suma = 0;
+
+                                for (int i = 0; i < grupo.Length; i++)
+                                {
+                                    suma += grupo[i].getPromedio();
+                                }
+
+                                double promedio =
+                                    suma / grupo.Length;
+
+                                Console.WriteLine(
+                                    "Promedio del grupo: "
+                                    + promedio.ToString("0.00")
+                                );
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine(
+                                "Presiona ENTER para continuar..."
+                            );
+                            Console.ReadLine();
+
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            Console.Clear();
+
+                            Console.WriteLine(
+                                "** ESTUDIANTES REGULARES **"
+                            );
+                            Console.WriteLine();
+
+                            bool encontrados = false;
+
+                            for (int i = 0; i < grupo.Length; i++)
+                            {
+                                if (grupo[i].Esregular())
+                                {
+                                    Console.WriteLine(
+                                        "Estudiante #" + (i + 1)
+                                    );
+
+                                    Console.WriteLine(
+                                        "Promedio: "
+                                        + grupo[i].getPromedio()
+                                    );
+
+                                    Console.WriteLine(
+                                        "----------------------------"
+                                    );
+
+                                    encontrados = true;
+                                }
+                            }
+
+                            if (!encontrados)
+                            {
+                                Console.WriteLine(
+                                    "No hay estudiantes regulares."
+                                );
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine(
+                                "Presiona ENTER para continuar..."
+                            );
+                            Console.ReadLine();
+
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            Console.Clear();
+
+                            Console.WriteLine(
+                                "** ESTUDIANTES IRREGULARES **"
+                            );
+                            Console.WriteLine();
+
+                            bool encontrados = false;
+
+                            for (int i = 0; i < grupo.Length; i++)
+                            {
+                                if (!grupo[i].Esregular())
+                                {
+                                    Console.WriteLine(
+                                        "Estudiante #" + (i + 1)
+                                    );
+
+                                    Console.WriteLine(
+                                        "Promedio: "
+                                        + grupo[i].getPromedio()
+                                    );
+
+                                    Console.WriteLine(
+                                        "----------------------------"
+                                    );
+
+                                    encontrados = true;
+                                }
+                            }
+
+                            if (!encontrados)
+                            {
+                                Console.WriteLine(
+                                    "No hay estudiantes irregulares."
+                                );
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine(
+                                "Presiona ENTER para continuar..."
+                            );
+                            Console.ReadLine();
+
+                            break;
+                        }
+
+                    case 6:
+                        Console.WriteLine(
+                            "Programa terminado."
+                        );
+                        break;
+
+                    default:
+                        Console.WriteLine(
+                            "Opcion no valida."
+                        );
+
+                        Console.WriteLine(
+                            "Presiona ENTER para continuar..."
+                        );
+                        Console.ReadLine();
+
+                        break;
+                }
+
+            } while (opcion != 6);
         }
     }
-
+}
